e17c10c224f7ee4854eadf920f2a5be5e3dd216c
Subham K <me@growly.gg>
2025-09-14 12:33:55 -0700

    chore: update

--- app/auth/login/route.ts
+++ app/auth/login/route.ts
@@ -2,7 +2,10 @@ import { getSignInUrl } from "@workos-inc/authkit-nextjs";
 import { redirect } from "next/navigation";
 
 export const GET = async () => {
-  const signInUrl = await getSignInUrl();
+  const baseUrl = 'https://refactor.liftgate.io';
+  const signInUrl = await getSignInUrl({
+    redirectUri: `${baseUrl}/dashboard`,
+  });
 
   return redirect(signInUrl);
 };
