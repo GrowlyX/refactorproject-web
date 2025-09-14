8d4aab7b262052808e68d4a57c905df79057fcc5
Subham K <me@growly.gg>
2025-09-14 13:01:53 -0700

    chore: dawggg

--- app/auth/login/route.ts
+++ app/auth/login/route.ts
@@ -2,9 +2,8 @@ import { getSignInUrl } from "@workos-inc/authkit-nextjs";
 import { redirect } from "next/navigation";
 
 export const GET = async () => {
-  const baseUrl = 'https://refactor.liftgate.io';
   const signInUrl = await getSignInUrl({
-    redirectUri: `${baseUrl}/dashboard`,
+    redirectUri: `https://refactor.liftgate.io/dashboard`,
   });
 
   return redirect(signInUrl);
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
-import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
+import {authkitMiddleware} from "@workos-inc/authkit-nextjs";
 
 export default authkitMiddleware({
-  middlewareAuth: {
-    unauthenticatedPaths: ["/", "/auth/login", "/auth/callback"],
-  },
+    middlewareAuth: {
+        enabled: true,
+        unauthenticatedPaths: ["/", "/auth/login", "/auth/callback"],
+    },
 });
 
 // Match against the pages
-export const config = { matcher: ["/:path*", "/account/:path*", "/api/:path*", "/dashboard"] };
+export const config = {matcher: ["/:path*", "/account/:path*", "/api/:path*", "/dashboard"]};
